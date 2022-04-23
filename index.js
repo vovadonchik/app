86504dc32b7ddb90ee36b863cdbfc46d37583336
Vladimir Kiselov <vovadonchik@gmail.com>
2022-04-23 04:58:49 +0300

    test

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const name = 'Vladimir'
-const postsQty = 26
-
-const userProfile = {
-    name,
-    postsQty,
-    human: true
+const myBosik = {
+    krasiviy: true,
+    friends: 'Vovchina, Levani',
+    city: 'Moscow',
+    cityGreeting() {
+        console.log('Privet Bosik')
+    }
 }
 
-console.log(userProfile)
+myBosik1 = myBosik
+
+myBosik1.mDenchik = 'kakashka'
+
+const name = 'Vovan'
+const serName = 'Kiselov'
+
+const vovadonchik = {
+    name,
+    serName,
+    age: 26
+}
+
+console.table(vovadonchik)
+
+console.table(myBosik1)
+
+myBosik.cityGreeting()
